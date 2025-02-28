964bdb62af98f5c9dbc2ce013c72cfd71cf43709
Abraham Briones <abebriones98@gmail.com>
2025-02-28 17:28:50 -0500

    utils updated

--- talent_data_pipeline/__init__.py
+++ talent_data_pipeline/__init__.py
@@ -5,3 +5,13 @@ from .transform.applicants import create_applicant_table
 from .transform.jobs import transform_jobs
 from .transform.offers import transform_offers
 from .utils import make_application_pairs
+
+__all__ = [
+    "Transformer",
+    "download_html_data",
+    "DataBase",
+    "create_applicant_table",
+    "transform_jobs",
+    "transform_offers",
+    "make_application_pairs",
+]
