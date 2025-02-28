2d6e0dccdbaf50f14fb7ce7075eba6051ff43f51
Abraham Briones <abebriones98@gmail.com>
2025-02-28 17:14:14 -0500

    utils updated

--- talent_data_pipeline/__init__.py
+++ talent_data_pipeline/__init__.py
@@ -4,3 +4,4 @@ from .load.sqlite_loader import DataBase
 from .transform.applicants import create_applicant_table
 from .transform.jobs import transform_jobs
 from .transform.offers import transform_offers
+from .utils import make_application_pairs
--- talent_data_pipeline/utils.py
+++ talent_data_pipeline/utils.py
@@ -1,2 +1,21 @@
-def make_application_pairs():
-    pass
+from pandas import DataFrame
+
+
+def make_application_pairs(transaction_frame: DataFrame) -> DataFrame:
+    """
+    Create a `Pair` column of concatenated "Job ID" and "Person ID".
+    :param transaction_frame: DataFrame containing the "Job ID" and "Person ID" column.
+    :return: DataFrame with a concatenation of "Job ID" and "Person ID" as "Pair" column.
+    """
+    if not {"Job ID", "Person ID"}.issubset(transaction_frame.columns):
+        print("Job ID and/or Person ID columns are missing in the dataframe.")
+        raise KeyError
+
+    transaction_pairs: DataFrame = transaction_frame.copy()
+    transaction_pairs["Pair"] = (
+        transaction_pairs["Job ID"].astype(str)
+        + "-"
+        + transaction_pairs["Person ID"].astype(str)
+    )
+
+    return transaction_pairs
